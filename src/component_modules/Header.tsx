a507bb3e0c2f3dda069148c1faebc93dbbcf0320
Raymond Zhuang <z5592481@ad.unsw.edu.au>
2026-05-21 13:41:13 +1000

    Created two buttons on the navigation bar

--- src/component_modules/Header.tsx
+++ src/component_modules/Header.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom"
 import pageStyles from "../css_modules/Page.module.css"
 import textStyles from "../css_modules/Text.module.css"
 import imageStyles from "../css_modules/Image.module.css"
+import buttonStyles from "../css_modules/Button.module.css"
 
 import nbaLogo from "../assets/nba-logo_brandlogos.net_ipeky.png"
 
@@ -27,6 +28,10 @@ const Header = () => {
           style={{background: "linear-gradient(to bottom left, #C9082A 50%, #1D428A 50%)"}}>
             <b>World</b>
         </h2>
+      </div>
+      <div>
+        <button className={buttonStyles["navbar"]}>Player H2H</button>
+        <button className={buttonStyles["navbar"]}>Guess the Player</button>
       </div>
     </div>
   )
