8b032c926c9191ecd173d75be239f7eb51af4b51
Raymond Zhuang <z5592481@ad.unsw.edu.au>
2026-05-21 13:11:56 +1000

    Created ste logo located in header

--- src/component_modules/Home.tsx
+++ src/component_modules/Home.tsx
@@ -1,13 +1,30 @@
-import nbaLogo from "../assets/National_Basketball_Association_logo.svg.png"
+import pageStyles from "../css_modules/Page.module.css"
+import textStyles from "../css_modules/Text.module.css"
+import imageStyles from "../css_modules/Image.module.css"
+
+import nbaLogo from "../assets/nba-logo_brandlogos.net_ipeky.png"
 
 const Home = () => {
   return (
     <>
-      <div style={{display: "flex", padding: "10px", backgroundColor: "#C9082A"}}>
-        <img src={nbaLogo} alt="" style={{height: "10vh"}}/>
+      <div className={pageStyles["header"]}>
+        <div className={pageStyles["header-logo"]}>
+          <h2
+            className={textStyles["header-logo"]}
+            style={{background: "linear-gradient(to bottom right, #C9082A 50%, #1D428A 50%)"}}>
+              <b>RAY-Z's</b>
+          </h2>
+          <img src={nbaLogo} alt="" className={imageStyles["header-logo"]}/>
+          <h2
+            className={textStyles["header-logo"]}
+            style={{background: "linear-gradient(to bottom left, #C9082A 50%, #1D428A 50%)"}}>
+              <b>World</b>
+          </h2>
+        </div>
+        
       </div>
-      <div style={{height: "80vh", backgroundColor: "#FFFFFF"}}></div>
-      <div style={{height: "10vh", backgroundColor: "#1D428A"}}></div>
+      <div className={pageStyles["page"]}></div>
+      <div className={pageStyles["footer"]}></div>
     </>
   )
 }
