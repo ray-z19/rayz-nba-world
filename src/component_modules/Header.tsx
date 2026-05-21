8631d1df49317befc00a26c634e51a9dcc6133ae
Raymond Zhuang <z5592481@ad.unsw.edu.au>
2026-05-21 13:23:00 +1000

    Refactored header into its own dedicated component

---
+++ src/component_modules/Header.tsx
@@ -0,0 +1,35 @@
+import { useNavigate } from "react-router-dom"
+
+import pageStyles from "../css_modules/Page.module.css"
+import textStyles from "../css_modules/Text.module.css"
+import imageStyles from "../css_modules/Image.module.css"
+
+import nbaLogo from "../assets/nba-logo_brandlogos.net_ipeky.png"
+
+const Header = () => {
+  const navigate = useNavigate();
+
+  const navigateHome = () => {
+    navigate("/");
+  }
+  
+  return (
+    <div className={pageStyles["header"]}>
+      <div className={pageStyles["header-logo"]} onClick={navigateHome}>
+        <h2
+          className={textStyles["header-logo"]}
+          style={{background: "linear-gradient(to bottom right, #C9082A 50%, #1D428A 50%)"}}>
+            <b>RAY-Z's</b>
+        </h2>
+        <img src={nbaLogo} alt="" className={imageStyles["header-logo"]}/>
+        <h2
+          className={textStyles["header-logo"]}
+          style={{background: "linear-gradient(to bottom left, #C9082A 50%, #1D428A 50%)"}}>
+            <b>World</b>
+        </h2>
+      </div>
+    </div>
+  )
+}
+
+export { Header }
--- src/component_modules/Home.tsx
+++ src/component_modules/Home.tsx
@@ -1,28 +1,11 @@
-import pageStyles from "../css_modules/Page.module.css"
-import textStyles from "../css_modules/Text.module.css"
-import imageStyles from "../css_modules/Image.module.css"
+import { Header } from "./Header"
 
-import nbaLogo from "../assets/nba-logo_brandlogos.net_ipeky.png"
+import pageStyles from "../css_modules/Page.module.css"
 
 const Home = () => {
   return (
     <>
-      <div className={pageStyles["header"]}>
-        <div className={pageStyles["header-logo"]}>
-          <h2
-            className={textStyles["header-logo"]}
-            style={{background: "linear-gradient(to bottom right, #C9082A 50%, #1D428A 50%)"}}>
-              <b>RAY-Z's</b>
-          </h2>
-          <img src={nbaLogo} alt="" className={imageStyles["header-logo"]}/>
-          <h2
-            className={textStyles["header-logo"]}
-            style={{background: "linear-gradient(to bottom left, #C9082A 50%, #1D428A 50%)"}}>
-              <b>World</b>
-          </h2>
-        </div>
-        
-      </div>
+      <Header />
       <div className={pageStyles["page"]}></div>
       <div className={pageStyles["footer"]}></div>
     </>
