183c104a7b4ae19fd0cf17ec06b2850c19e08bb3
Raymond Zhuang <z5592481@ad.unsw.edu.au>
2026-05-23 21:35:26 +1000

    Inserted image on landing page

--- src/component_modules/Home.tsx
+++ src/component_modules/Home.tsx
@@ -2,9 +2,15 @@ import { Page } from "./Page"
 
 import pageStyles from "../css_modules/Page.module.css"
 
+import homeBanner from "../assets/sas-v-okc.jpg"
+
 const Home = () => {
   return (
-    <Page content={<div className={pageStyles["page"]}>Home</div>}/>
+    <Page content={
+      <div className={pageStyles["page"]}>
+        <img src={homeBanner} alt="" style={{width: "1000px", margin: "50px 0px"}} />
+      </div>
+    }/>
   )
 }
 
